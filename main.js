af034520c5b7394324dad08821973ceac9b3ff8a
Silly-Snake <82917215+Silly-Snake@users.noreply.github.com>
2023-12-05 21:48:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function astronomy(){
+    window.location="astronomy.html";
+}
+function biology(){
+    window.location="biology.html";
+}
+function physics(){
+    window.location="physics.html";
+}
+function chemistry(){
+    window.location="chemistry.html";
+}
+function psychology(){
+    window.location="psychology.html";
+}
